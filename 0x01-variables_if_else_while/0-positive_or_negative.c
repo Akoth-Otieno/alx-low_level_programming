011e91e6706d2d40f633490befff9f4a9a0e63b4
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-03 07:15:08 -0700

    update program to conform with betty rules

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,31 +3,31 @@
 #include <time.h>
 
 /**
- * main - Prints a random number and returns if the number 
- *	is negative, zero or positive.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints a random number and returns if the number 
+*	is negative, zero or positive.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
 
-	else if (n < 0)
-	{
-	printf("%d is negative\n", n);
-	}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
 
-	else
-	{
-		printf("%d is zero\n", n);
-	}
-	return (0);
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
