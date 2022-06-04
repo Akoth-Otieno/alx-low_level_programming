9bd386030d2fab304770d210e87c86aa2527d6d9
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-04 08:15:39 -0700

    write a program that prints out the letters of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints the alphabet in upper and lowercase.
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+
+char A;
+for (A = 'A'; a <= 'Z'; A++)
+{
+putchar (A);
+}
+putchar ('\n')
+
+return (0);
+}
