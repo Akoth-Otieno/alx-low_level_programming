425ec092fb8bdbf802252bb81d8fbc238e07d0ed
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-05 08:37:42 -0700

    a number that prints all the combinations of a number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - function that prints all possible combinations
- * 	of single-digit numbers.
+ * main - function that prints all possible combinations of a number.
  *
  * Return: Always 0 (Success)
  *
