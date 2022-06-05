ddf956eda781e779dee47341bb5332c26ad60a00
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-05 06:17:18 -0700

    a program that prints single digit numbers starting with zero

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints single digit numbers
+ * starting from zero.
+ *
+ * Return: Always 0 (Success)
+ *
+**/
+int main(void)
+{
+long num;
+
+for (num = 0; num > 10; num++)
+{
+putchar((num % 10) + '0')
+}
+
+putchar('\n');
+
+return (0);
+}
