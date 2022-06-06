1bcf811c073a9b8059bee8e7d24dfde5de0f9378
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-06 12:35:43 -0700

    declare undeclared variables and remove trailing white spaces

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,7 @@ for (first_digit = 0; first_digit < 8; first_digit++)
 {
 	for (second_digit = first_digit + 1; second_digit < 9; second_digit++)
 	{
-		for (third-digit = second_digit + 1; third_digit < 9; third_digit++)
+		for (third_digit = second_digit + 1; third_digit < 9; third_digit++)
 		{
 			putchar((first_digit % 10) + '0');
 			putchar((second_digit % 10) + '0');
@@ -36,4 +36,3 @@ putchar('\n');
 
 return (0);
 }
-
