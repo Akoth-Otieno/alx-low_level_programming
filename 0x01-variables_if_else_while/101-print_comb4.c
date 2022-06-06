a00335685b924cf35673575093f64cb1e6b8116a
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-06 12:29:57 -0700

    a program that prints all the combinations of three-digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints out all permutations of
+ *	all three-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+int first_digit;
+int second_digit;
+int third_digit;
+
+for (first_digit = 0; first_digit < 8; first_digit++)
+{
+	for (second = first_digit + 1; second_digit < 9; second_digit++)
+	{
+		for (third-digit = second_digit + 1; third_digit < 9; third_digit++)
+		{
+			putchar((first_digit % 10) + '0');
+			putchar((second_digit % 10) + '0');
+			putchar((third_digit % 10) + '0');
+
+			if (first_digit == 7 && second_digit == 8 && third-digit == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+putchar('\n');
+
+return (0);
+}
+
