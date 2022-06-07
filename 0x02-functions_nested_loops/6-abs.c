f92e3eb9c7974817ce80d29c3db28f05c2154306
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-07 16:27:31 -0700

    write a function that computes the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - function that prints the absolute value of a number.
+ *
+ * @int: the number whose absolute value is tested
+ *
+ * Return: absolute value of a number
+ *
+*/
+int _abs(int)
+{
+int a;
+
+if (i < 0)
+{
+	i = i * -1;
+}
+
+return (i);
+}
