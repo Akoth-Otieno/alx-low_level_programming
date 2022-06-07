6b56a4856eeb7d7dfdb64342a2d8810e74eda071
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-07 09:21:54 -0700

    rewrite statements

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,22 +10,13 @@
 */
 int main(void)
 {
-char _;
-char p;
-char u;
-char t;
-char c;
-char h;
-char a;
-char r;
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
-	putchar('\n');
+char c[8] = "_putchar";
+int i;
+
+for (i = 0; i < 9; i++)
+{
+	_putchar(c[i]);
+}
+	_putchar('\n');
 return (0);
 }
