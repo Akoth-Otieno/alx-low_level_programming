6815fb7cae8fa6a3c8f6b9f3aaa185312bb16249
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-08 07:20:23 -0700

    write a function that prints the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a: one of the variables that is added to another variable
+ * @b: second of the variables to be added to the first one
+ *
+ * Return: results of the addition of two numbers
+ *
+*/
+
+int add(int, int)
+{
+int a;
+int b;
+int sum;
+
+sum = a +b;
+
+return (sum);
+
+_putchar('\n');
+}
