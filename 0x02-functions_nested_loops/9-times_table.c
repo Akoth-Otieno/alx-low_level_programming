c315334ac0d29c58fb8a212169f0b17c8ffef868
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-08 07:03:57 -0700

    remove trailing white spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,11 @@ for (nine = 0; nine <= 9; nine++)
 	for (i = 0; i <= 9; i++)
 	{
 	product = (nine * i);
-	
 	if (i != 0)
 	{
 		_putchar(',');
 		_putchar(' ');
 	}
-	
 	if (product >= 10)
 	{
 		_putchar((product / 10) + '0');
@@ -37,7 +35,6 @@ for (nine = 0; nine <= 9; nine++)
 		_putchar(' ');
 		_putchar((product % 10) + '0');
 	}
-	
 	else
 		_putchar((product % 10) + '0');
 	}
