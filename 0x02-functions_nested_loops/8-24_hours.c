f7bba36b4f9053437087895116802d5ee6646b63
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-08 04:01:00 -0700

    add the prototype of the function that prints out every minute in 24 hours to the header file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,18 @@
 **/
 void jack_bauer(void)
 {
-int minutes;
+float minutes;
 float hours;
 
 for (hours = 00.00; hours <= 23.59; hours++)
 {
-	minutes = hours*60;
-	_putchar((minutes % 10) + '0');
+	for (minutes = 0; minutes <= 60; minutes++)
+	{
+		_putchar((hours / 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+		_putchar ('\n');
+
 }
 
 _putchar('\n');
