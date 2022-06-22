584f38654dff70f5c5dfbe5752d2f94fdbbb44ba
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-22 06:17:37 -0700

    remove trailing white spaces

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,14 +11,14 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
 		return;
 	}
-
-	else 
+	
+	else
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 
