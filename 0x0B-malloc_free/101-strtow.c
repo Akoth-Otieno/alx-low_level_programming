090ad81e1c78b469436df944bb2bd5aebee251f6
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-29 08:03:38 -0700

    write a script that splits a string into 2

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - a function that splits a string into two words
+ *
+ * @str - the input string to be split
+ *
+ * Return: A pointer to an array of strings or NULL (if the
+ * string is empty).
+ *
+*/
+
+char **strtow(char *str)
+{
+
+}
