602b94c1850d585872f5adde2949467d783fa925
AKoth-Otieno <elsyakoth@gmail.com>
2022-06-30 06:31:27 -0700

    update the logic for the malloc function

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,18 +13,17 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr_b;
 	
-	ptr_b = malloc(sizeof(b));
+	unsigned int ptr_b = malloc(b);
 
-	if (ptr_b !=  NULL)
+	if (ptr_b == NULL)
 	{
-		return (ptr_b);
+		exit(98);
 	}
 
 	else
 	{
-		exit (98);
+		return (ptr_b);
 	}
 
 }
