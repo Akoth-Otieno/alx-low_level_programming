a387035177cee3d0a37b86defeceefd9f7b69ce2
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-06 07:00:48 -0700

    write a function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,31 +1,18 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#define secure_free(f) (free(f), (f) = 0)
 
 /**
  * print_name - a function that prints a name.
  * @name: the pointer to the character variable to be printed.
- * @f: a pointer to a character
+ * @f: a pointer to a function
  * Return: void
 */
 
 
 void print_name(char *name, void (*f)(char *))
 {
-	while(*name != '\0')
-	{
-		_putchar(*name);
-		name++;
-	}
+	if (f != NULL)
+		f(name);
 }
 
-/**
- * main - function that contains a pointer to a function.
- * @f: the pointer to a function.
- * Return: void
- */
-
-void main ()
-{
-	void (*f)(char *);
-	f = print_name;
-}
