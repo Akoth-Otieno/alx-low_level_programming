9d043334e67002d6f401b2e13b81d32d1dd8e9b7
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-13 23:52:19 -0700

    correct syntax errors.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,12 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-                _putchar('\n');
+		_putchar('\n');
 
 
-	for (i = 1; i <= n; i++)
+	for (i = 1; i < n; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
