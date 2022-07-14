3bb188fdc1b901a0ae91b98523853105732cd041
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-14 02:19:47 -0700

    Write a fizz buzz program.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints a predetermined output to the terminal.
+ *
+ * Description - print Fizz if a num ber is a multiple of 3, Buzz if a number is
+ * a multiple of 5, FizzBuzz if a number is a multiple of 3 and 5,
+ * otherwise print the number.
+ *
+ * Return: void
+ */
+
+void main (void)
+{
+	int num;
+	
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", num);
+
+		if (num < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	
+}
