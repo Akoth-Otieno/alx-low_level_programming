1be68443436fabcdc0cfcc2a277cd5c90288095e
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-14 02:35:06 -0700

    Write a fizz buzz program.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,26 +11,23 @@
  * Return: void
  */
 
-void main (void)
+int main(void)
 {
 	int num;
-	
+
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
-		else if (num % 5 == 0)
-			printf("Buzz");
-		else if (num % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%d", num);
-
+			printf("%i", i);
 		if (num < 100)
 			printf(" ");
-		else
-			printf("\n");
 	}
-
-	
+	printf("\n");
+	return (0);
 }
