6f00183729a8f2706f76931aa89fb0f1241ea268
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-19 03:15:28 -0700

    remove a trailing white space in line 4.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of elements in 
- * a linked list.
+ * listint_len - a function that returns the number of elements
+ * in a linked list.
  * @h: the first node in the list.
  * Return: number of nodes.
  */
