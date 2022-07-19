d1fe1046d4941defb058e7133bafabc0e6fcd741
AKoth-Otieno <elsyakoth@gmail.com>
2022-07-19 15:00:00 -0700

    create project file.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - a function that prints a linked list.
+ * @head: the first node in the list.
+ * Return: number of nodes in the list.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+
+}
