7d9297cbe39b3055263359aa15fa5ac3176fe750
AKoth-Otieno <elsyakoth@gmail.com>
2022-09-23 05:32:52 -0700

    add a description for the hash function.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
-/*
- * hash_table_t - function that creates a hash table.
- * @size: size of the array
- * Return: pointer to the hash table or NULL (incase of error)
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
